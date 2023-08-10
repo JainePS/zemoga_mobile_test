490519588acd263abe6a88c21d4dca1dcd5e0a92
jaine-valari <jaine@valarisolutions.com>
2023-08-10 22:24:41 +0200

    feat: pass the post data to the feeed template on home component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState } from 'react';
+import FeedTemplate from '../../components/templates/FeedTemplate';
+import usePosts from '../../hooks/usePosts';
+
+const Home = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+  const {getPosts} = usePosts();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const newPosts = await getPosts();
+      console.log('carnsal', newPosts);
+      setPosts(newPosts as any);
+    };
+
+    fetchPosts();
+  }, []);
+
+  return <FeedTemplate posts={posts} />;
+};
+
+export default Home;
