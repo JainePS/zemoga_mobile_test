00e079e6751f2bf85b98cb6e9f8ac3b002c833a7
jaine-valari <jaine@valarisolutions.com>
2023-08-15 00:02:47 +0200

    fix: declare on delete function as promise

--- src/components/templates/PostDetailsTemplate.tsx
+++ src/components/templates/PostDetailsTemplate.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+import {Text, View} from 'react-native';
 import {Post} from '../../types/Post';
 import {User} from '../../types/User';
 import {Comment} from '../../types/Comment';
@@ -8,14 +8,14 @@ import styles from '../../layouts/postDetailsStyles';
 import PostHeader from '../organisms/PostHeader';
 import UserInfo from '../molecules/UserInfo';
 import PostComments from '../organisms/Comment';
-import {favoritePosts} from '../../utils/favorites';
+// import {favoritePosts} from '../../utils/favorites';
 
 type Props = {
   post?: Post;
   user?: User;
   comments?: Comment[];
-  onDelete?: (id: number) => void;
-  onFavoritePost?: (postId: number) => void;
+  onDelete?: (id: number) => Promise<void>;
+  onFavoritePost?: (postId: number) => Promise<void>;
 };
 
 const PostDetailsTemplate = ({
@@ -25,15 +25,17 @@ const PostDetailsTemplate = ({
   onDelete,
   onFavoritePost,
 }: Props) => {
-  console.log('user', user);
+  if (!post) {
+    return <Text>Post not found</Text>;
+  }
 
   return (
-    <SafeAreaView style={styles.postContainer}>
+    <View style={styles.postContainer}>
       <PostHeader post={post} onDelete={onDelete} onFavorite={onFavoritePost} />
       <Text style={styles.postText}>{post?.body}</Text>
       <UserInfo user={user} />
       <PostComments comments={comments} />
-    </SafeAreaView>
+    </View>
   );
 };
 
