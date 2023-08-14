ac1941056bd7e7ee1111d7ab509a252a8e96d195
jaine-valari <jaine@valarisolutions.com>
2023-08-14 16:47:07 +0200

    feat: create scg icons and tooltip component

--- src/components/icons/StarIcon.tsx
+++ src/components/icons/StarIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import Svg, {Path} from 'react-native-svg';
 import {SVGProps} from '../../types/SvgProps';
 
@@ -6,11 +6,13 @@ const StarIcon = ({fill = 'black', width = 20, height = 20}: SVGProps) => {
   return (
     <Svg width={width} height={height} viewBox={'0 0 122 145'} fill={fill}>
       <Path
-        d="M14.1,0.43l3.44,8.05l8.72,0.78c0.39,0.03,0.67,0.37,0.64,0.76c-0.02,0.19-0.1,0.35-0.24,0.47l0,0 l-6.6,5.76l1.95,8.54c0.09,0.38-0.15,0.75-0.53,0.84c-0.19,0.04-0.39,0-0.54-0.1l-7.5-4.48l-7.52,4.5 c-0.33,0.2-0.76,0.09-0.96-0.24c-0.1-0.16-0.12-0.35-0.08-0.52h0l1.95-8.54l-6.6-5.76c-0.29-0.25-0.32-0.7-0.07-0.99 C0.3,9.35,0.48,9.28,0.66,9.27l8.7-0.78l3.44-8.06c0.15-0.36,0.56-0.52,0.92-0.37C13.9,0.13,14.03,0.27,14.1,0.43L14.1,0.43 L14.1,0.43z"
-        fill={fill}
+        d="M19.8 9.8L14.3 9L11.9 4L9.5 9L4 9.8L7.9 13.6L7 19L11.9 16.5L16.8 19.1L15.9 13.6L19.8 9.8ZM11.9 15.4L8.3 17.3L9 13.3L6.1 10.5L10.1 9.9L11.9 6.3L13.7 9.9L17.7 10.5L14.8 13.3L15.5 17.3L11.9 15.4Z"
+        fill="#1C2E45"
       />
     </Svg>
   );
 };
 
-export default StarIcon;
+const star = memo(StarIcon);
+
+export default star;
