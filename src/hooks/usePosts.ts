113f242fda25e9e9ef9d7c51fe7aa0d2a635b915
jaine-valari <jaine@valarisolutions.com>
2023-08-14 16:45:37 +0200

    feat: add fetch methods to the posts doc

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,17 +1,74 @@
-import {fetchPosts} from '../services/posts';
-
+import {
+  deletePost,
+  fetchPostById,
+  fetchPostByUser,
+  fetchPostComments,
+  fetchPosts,
+} from '../services/posts';
+import {Comment} from '../types/Comment';
+import {Post} from '../types/Post';
+//TODO: handle errors
 const usePosts = () => {
-  const getPosts = async () => {
+  const getPosts = async (): Promise<Post[]> => {
     try {
       const posts = await fetchPosts();
       const postsJson = await posts.json();
-      return postsJson as any;
+      return postsJson as Post[];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const getPostById = async (postId: number) => {
+    try {
+      const posts = await fetchPostById(postId);
+      const postsJson = await posts.json();
+      return postsJson as Post;
     } catch (error) {
       console.log(error);
     }
   };
 
-  return {getPosts};
+  const getPostByUser = async (userId: number) => {
+    try {
+      const posts = await fetchPostByUser(userId);
+      const postsJson = await posts.json();
+      return postsJson as Post[];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPostComments = async (
+    postId: number,
+  ): Promise<Comment[] | undefined> => {
+    try {
+      const postComments = await fetchPostComments(postId);
+      const postCommentsJson = await postComments.json();
+      return postCommentsJson;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deletePostByPostId = async (userId: number) => {
+    try {
+      const response = await deletePost(userId);
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    getPosts,
+    getPostByUser,
+    getPostById,
+    getPostComments,
+    deletePostByPostId,
+  };
 };
 
 export default usePosts;
--- src/services/posts.ts
+++ src/services/posts.ts
@@ -6,4 +6,35 @@ const fetchPosts = async () => {
   });
 };
 
-export {fetchPosts};
+const fetchPostById = async (postId: number) => {
+  console.log(postId, `${URL}/${postId}`);
+
+  return await fetch(`${URL}/${postId}`, {
+    method: 'GET',
+  });
+};
+
+const fetchPostByUser = async (userId: number) => {
+  return await fetch(`${URL}/${userId}`, {
+    method: 'GET',
+  });
+};
+
+const fetchPostComments = async (postId: number) => {
+  return await fetch(`${URL}/${postId}/comments`, {
+    method: 'GET',
+  });
+};
+
+const deletePost = async (postId: number) => {
+  return fetch(`${URL}/${postId}`, {
+    method: 'DELETE',
+  });
+};
+export {
+  fetchPosts,
+  fetchPostByUser,
+  fetchPostById,
+  fetchPostComments,
+  deletePost,
+};
